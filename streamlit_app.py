3f1653c1167fccdf852fa9874a6091f984c390fc
Shobhit Srivastava <37845662+shobuntu@users.noreply.github.com>
2024-05-06 04:48:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,36 @@ import streamlit as st
 
 def main():
     st.title("My Resume")
-    st.header("John Doe")
-    st.subheader("Software Developer")
+    name = st.text_input("Name", "John Doe")
+    profession = st.text_input("Profession", "Software Developer")
+
+    st.header(name)
+    st.subheader(profession)
 
     st.write("""
-        Hello! I'm a software developer passionate about creating useful and efficient software solutions. 
-        I have experience in various programming languages and technologies, and I'm always eager to learn new things.
-    """)
+        Hello! I'm {} and I work as a {}.
+        """.format(name, profession))
 
     st.subheader("Experience")
+    exp1 = st.text_area("Experience 1", "Software Developer at XYZ Company (2019 - Present)")
+    exp2 = st.text_area("Experience 2", "Internship at ABC Tech (2018)")
     st.write("""
-        - Software Developer at XYZ Company (2019 - Present)
-        - Internship at ABC Tech (2018)
-    """)
+        - {}
+        - {}
+        """.format(exp1, exp2))
 
     st.subheader("Education")
-    st.write("""
-        - Bachelor's Degree in Computer Science, University of XYZ (2015 - 2019)
-    """)
+    edu = st.text_area("Education", "Bachelor's Degree in Computer Science, University of XYZ (2015 - 2019)")
+    st.write("- {}".format(edu))
 
     st.subheader("Skills")
-    st.write("""
+    skills = st.text_area("Skills", """
         - Programming Languages: Python, JavaScript, Java
         - Web Development: HTML, CSS, Flask, Django
         - Database: SQL, MongoDB
         - Tools & Technologies: Git, Docker, AWS
-    """)
+        """)
+    st.write(skills)
 
 if __name__ == "__main__":
     main()
